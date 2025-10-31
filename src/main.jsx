65d1345b6bbf175f7a4f3b4682f468cc7b2f3bf0
Tahmina Akter <tahminaakter@Tahminas-MacBook-Air.local>
2025-10-31 17:15:30 +0600

    folder structure change

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "../../src/App.css";
+import "./App.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import About from "./about/About.jsx";
-import Home from "./home/Home.jsx";
-import Shop from "./shop/Shop.jsx";
-import Contact from "./contact/Contact.jsx";
+import About from "./pages/about/About.jsx";
+import Home from "./pages/home/Home.jsx";
+import Shop from "./pages/shop/Shop.jsx";
+import Contact from "./pages/contact/Contact.jsx";
 
 const router = createBrowserRouter([
   {
